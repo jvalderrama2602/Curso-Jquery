327a0eb889699c07d2e285f3f90780b39c1c4a9a
José Valderrama <Jos Valderrama>
2016-11-15 15:58:47 -0400

    Ejmeplo con ajax en jquery

---
+++ colores.php
@@ -0,0 +1,16 @@
+<?php
+if($_GET['color']=="verde"){
+
+	echo "Presionaste verde";
+}
+if($_GET['color']=="azul"){
+
+	echo "Presionaste azul";
+}
+if($_GET['color']=="rojo"){
+
+	echo "Presionaste rojo";
+}
+
+
+?>
